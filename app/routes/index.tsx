47e9437bbd54e2184596d53e4391852cd8892e1a
finnianmacken <finn@minerva.kgi.edu>
2022-05-07 19:33:19 -0700

    Connected Redis to the application

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,18 @@ import type {
   LoaderFunction,
 } from "@remix-run/node";
 import { redirect } from '@remix-run/node';
+import { redis } from "~/models/redis.server"
 
 export const loader: LoaderFunction = async () => {
+
+  const start = Date.now()
+
+  const testData = await redis.get("big test data")
+  const end = Date.now() - start
+  console.log("END TIME:", end)
+
+  console.log(testData)
+
   return redirect("/dashboard");
+
 };
