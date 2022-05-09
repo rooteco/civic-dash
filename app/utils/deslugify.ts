1e1bf481d90316030e6c47be2c34fe2a975f2397
finnianmacken <finn@minerva.kgi.edu>
2022-05-09 12:44:14 -0700

    Fixed a bug in deslugify

--- app/utils/deslugify.ts
+++ app/utils/deslugify.ts
@@ -1,6 +1,7 @@
 export function deslugify(slug: string) : string {
   const str = slug.replace("-", " ")
    .split(' ')
+   .filter(word => word.trim().length > 0) // captures error when you have trailing/leading dashes
    .map(w => w[0].toUpperCase() + w.substring(1).toLowerCase())
    .join(' ');
    return str
