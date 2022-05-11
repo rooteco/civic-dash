a178c12651b8b7415c02ef5b6d442fd6bb235925
finnianmacken <finn@minerva.kgi.edu>
2022-05-11 16:43:29 -0700

    Implemented basic working version of carousel

--- app/components/dashboard/theme-carousel-components/index-carousel.tsx
+++ app/components/dashboard/theme-carousel-components/index-carousel.tsx
@@ -11,40 +11,29 @@ export function IndexCarousel(props: CarouselProps){
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
+      breakpoint: { max: 4000, min: 0 },
       items: 2
     },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 2
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 2
-    }
   };
 
 
   return(
-    <div style={{border: "2px solid black", width: "200px", height: "200px"}}>
-      <Carousel responsive={responsive}>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
+      <Carousel
+        responsive={responsive}
+        autoPlay={false}
+        itemClass="ThemeButton"
+        containerClass="DashboardCarouselLibraryWrapper"
+        centerMode={true}
+        sliderClass="DashboardCarouselLibrarySlider"
+        shouldResetAutoplay={false}
+        >
+        {props && props.themes.map((theme)=>(
+            <div key={theme.id}>
+              <Link to={`/dashboard/theme/${theme.slug}`}>
+              <p>{theme.name}</p>
+              </Link>
+            </div>
+        ))}
       </Carousel>
-    </div>
   )
 }
-
-// {props && props.themes.map((theme)=>(
-//     <div key={theme.id} className="ThemeButton">
-//       <Link to={`/dashboard/theme/${theme.slug}`}>
-//       <p>{theme.name}</p>
-//       </Link>
-//     </div>
-// ))}
