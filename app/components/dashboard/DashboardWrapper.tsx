3565c9aa34a629b7f2385e6b845ae4e11e9ff5c8
andrepology <andrevacha@gmail.com>
2022-05-13 12:00:56 -0700

    style: refine grid and styling

--- app/components/dashboard/DashboardWrapper.tsx
+++ app/components/dashboard/DashboardWrapper.tsx
@@ -14,56 +14,48 @@ export function DashboardWrapper(props: WrapperProps){
   const [tableOpen, setTableOpen] = React.useState(true)
   return(
     <>
-    <TableOpenContext.Provider value={tableOpen}>
-    <div className="DashboardLinks">
-      <Link to={"/dashboard"}><p>Dashboard</p></Link>
-      <div><a
-              href="https://rooteco.notion.site/CivicDash-v2-1-8631ce9d3fd442a1a031eec9fa48cf3b"
-              target="_blank">Docs</a></div>
-            <div><a href="https://discord.gg/bQHFfWUC" target="_blank">Discord</a></div>
-    </div>
-    <div className="min-h-screen flex flex-row">
-      <div className="dashgrid">
-        <div className="axis"></div>
-        <div className="focus">
-          {props.focusChild}
-        </div>
-        <div className="header">
-            <p>Hello <strong>Farnney the Dinosaur</strong></p>
-            <div className="portfoliostats">
-              <button
-                  className="metric"
-                  onClick={()=>setTableOpen(prevState => !prevState)}>$1000</button>
-                <button
-                  className="metric"
-                  onClick={()=>setTableOpen(prevState => !prevState)}>3</button>
-        </div>
-        <div className="dash">
-          <div className="picker">
-            {props.themeCarouselChild}
-          </div>
-          <div className="sparklinecarousel">
-            {props.linkChild}
-          </div>
-        </div>
-        <div className="DashboardInset" id="ModalAnchor">
-          {props.predictionChild ? props.predictionChild : ""}
+      <TableOpenContext.Provider value={tableOpen}>
+        <div className="min-h-screen flex flex-row">
           <div className="DashboardLinks">
             <Link to={"/dashboard"}><p>Dashboard</p></Link>
             <div><a
                     href="https://rooteco.notion.site/CivicDash-v2-1-8631ce9d3fd442a1a031eec9fa48cf3b"
-                    target="_blank">Docs</a></div>
-                  <div><a href="https://discord.gg/bQHFfWUC" target="_blank">Discord</a></div>
+                    target="_blank" rel="noreferrer">Docs</a></div>
+                  <div><a href="https://discord.gg/bQHFfWUC" target="_blank" rel="noreferrer">Discord</a></div>
+          </div>
+          <div className="dashgrid">
+            <div className="axis"/>
+            <div className="focus">
+              {props.focusChild}
+            </div>
+            <div className="header">
+                <p>Hello <strong>Farnney the Dinosaur</strong></p>
+                <div className="portfoliostats">
+                  <button
+                      className="metric"
+                      onClick={()=>setTableOpen(prevState => !prevState)}>$1000</button>
+                    <button
+                      className="metric"
+                      onClick={()=>setTableOpen(prevState => !prevState)}>3</button>
+                </div>
+            </div>
+            <div className="dash">
+              <div className="picker">
+                {props.themeCarouselChild}
+              </div>
+              <div className="sparklineticker">
+                {props.linkChild}
+              </div>
+            </div>
+            <div className="DashboardInset" id="ModalAnchor">
+              {props.predictionChild ? props.predictionChild : ""}
+            </div>
           </div>
         </div>
-      </div>
-    </div>
     </TableOpenContext.Provider>
     </>
   )
 }
-
-
 
 
 // <Link><p>Docs</p></Link>
