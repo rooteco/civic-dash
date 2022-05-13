ed90048c47085aba8e9d816104825986b1ebb437
andrepology <andrevacha@gmail.com>
2022-05-13 08:42:40 -0700

    style: update names and grid

--- app/components/dashboard/DashboardWrapper.tsx
+++ app/components/dashboard/DashboardWrapper.tsx
@@ -17,42 +17,41 @@ export function DashboardWrapper(props: WrapperProps){
     <>
     <TableOpenContext.Provider value={tableOpen}>
     <div className="min-h-screen flex flex-row">
-      <div className="DashboardGrid">
-        <div className="DashboardFocus">
+      <div className="dashgrid">
+        <div className="axis"></div>
+        <div className="focus">
           {props.focusChild}
         </div>
-        <div className="DashboardThemeSelection">
-          <div className="DashboardThemeSelectionWelcomeWrapper">
-            <div className="DashboardHello">
-              <p>Hello <strong>Farnney the Dinosaur</strong></p>
-            </div>
-            <div className="DashboardFlexStretch" />
-            <div className="DashboardPredictionMetrics">
+        <div className="header">
+            <p>Hello <strong>Farnney the Dinosaur</strong></p>
+            <div className="portfoliostats">
               <button
-                  className="DashboardMetricWrapper"
+                  className="metric"
                   onClick={()=>setTableOpen(prevState => !prevState)}>$1000</button>
                 <button
-                  className="DashboardMetricWrapper"
+                  className="metric"
                   onClick={()=>setTableOpen(prevState => !prevState)}>3</button>
-            </div>
           </div>
-          <div className="DashboardThemeCarousel">
+        </div>
+        <div className="dash">
+          <div className="picker">
             {props.themeCarouselChild}
           </div>
+          <div className="sparklinecarousel">
+            {props.linkChild}
+          </div>
         </div>
-        <div className="DashboardCarousel">
-          {props.linkChild}
+        <div className="DashboardInset" id="ModalAnchor">
+          {props.predictionChild ? props.predictionChild : ""}
+          <div className="DashboardLinks">
+            <Link to={"/dashboard"}><p>Dashboard</p></Link>
+            <div><a
+                    href="https://rooteco.notion.site/CivicDash-v2-1-8631ce9d3fd442a1a031eec9fa48cf3b"
+                    target="_blank">Docs</a></div>
+                  <div><a href="https://discord.gg/bQHFfWUC" target="_blank">Discord</a></div>
+          </div>
         </div>
-      </div>
-      <div className="DashboardInset" id="ModalAnchor">
-        {props.predictionChild ? props.predictionChild : ""}
-        <div className="DashboardLinks">
-          <Link to={"/dashboard"}><p>Dashboard</p></Link>
-          <div><a
-                  href="https://rooteco.notion.site/CivicDash-v2-1-8631ce9d3fd442a1a031eec9fa48cf3b"
-                  target="_blank">Docs</a></div>
-                <div><a href="https://discord.gg/bQHFfWUC" target="_blank">Discord</a></div>
-        </div>
+
       </div>
     </div>
     </TableOpenContext.Provider>
