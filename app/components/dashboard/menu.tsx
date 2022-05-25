f30a7bd9fcfa0823c5d1f1646d2878d33c5f43c8
andrepology <andrevacha@gmail.com>
2022-05-25 13:52:10 -0700

    add variable fonts/stack and fix intros

--- app/components/dashboard/menu.tsx
+++ app/components/dashboard/menu.tsx
@@ -1,10 +1,29 @@
 import { Link } from '@remix-run/react';
+import { useState } from 'react';
 
 
 export default function Menu() {
-  return <div className="menu flex">
-            <Link to={"/dashboard"}>dash</Link>
-            <div><a href="https://rooteco.notion.site/CivicDash-v2-1-8631ce9d3fd442a1a031eec9fa48cf3b" target="_blank" rel="noreferrer">docs</a></div>
-            <div><a href="https://discord.gg/bQHFfWUC" target="_blank" rel="noreferrer">discord</a></div>
-          </div>;
+
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  }
+
+  if (open) {
+    return (
+      <div className="menu flex-row ">
+        <div className="icon-s" onClick={handleClick}/>
+        <div className="flex-row">
+          <Link to={"/dashboard"}>dash</Link>
+          <a href="https://rooteco.notion.site/CivicDash-v2-1-8631ce9d3fd442a1a031eec9fa48cf3b" target="_blank" rel="noreferrer">docs</a>
+          <a href="https://discord.gg/bQHFfWUC" target="_blank" rel="noreferrer">discord</a>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="menu flex-row icon-s" onClick={handleClick}/>
+  )
 }
