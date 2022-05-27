2a0097f23fa1aa600efabec16df5687c0361117a
andrepology <andrevacha@gmail.com>
2022-05-26 17:55:53 -0700

    update ticker chart and layout

--- app/components/dashboard/linking-components/IndicatorBox.tsx
+++ app/components/dashboard/linking-components/IndicatorBox.tsx
@@ -15,33 +15,32 @@ type IndicatorBoxProps = {
 
 export function IndicatorBox(props: IndicatorBoxProps){
   // TODO ANDRE: ICON-S IS DE-CENTERING THE HEARTS
-  // TODO ANDRE: CHANGE THE COLOUR SCHEME OF THE HEARTS
   return(
-    <div className = "indicator-sparkline flex" style={props.style}>
+    <Link to={props.linkString}>
+      <div className = "indicator-sparkline flex" style={props.style}>
 
-        <div className="indicator-sparkline-metadata">
-            <Link to={props.linkString}>
-              <p className="inscription truncate">{props.indicator.name}</p>
+          <div className="indicator-sparkline-metadata">
+              <p className="inscription">{props.indicator.name}</p>
 
               <h3>{Math.round(Math.random()*100)}</h3>
               <p className="inscription">{Math.round(Math.random()*100)}%</p>
-            </Link>
-        </div>
+            
+          </div>
 
         <div className="sparkline">
           <img src={graph ? graph : ""} alt="graph" className = "sparkline"/>
         </div>
 
-        <div className = "pad" style={{position: 'absolute'}}>
+        <div className = "pad" style={{position: 'absolute', right: '0px', bottom: '0px'}}>
           <Form method="post" >
             <input type="hidden" name="indicatorSlug" value={props.indicator.slug}/>
             <input type="hidden" name="isFavourited" value={props.favouritedIndicatorSlugs.some(obj => obj.slug === props.indicator.slug)}/>
             <input type="hidden" name="userId" value={props.user ? props.user.id : ""}/>
-            <button className = "icon-s" type="submit" style={{color: props.favouritedIndicatorSlugs.some(obj => obj.slug === props.indicator.slug) ? "white" : "grey"}}><FavoriteIcon fontSize="6px"/></button>
+            <button type="submit" style={{color: props.favouritedIndicatorSlugs.some(obj => obj.slug === props.indicator.slug) ? "white" : "grey"}}><FavoriteIcon fontSize="6px" sx={{ stroke: "#4b4b4b", strokeWidth: 1, color: "rgba(0,0,0,0)" }}/></button>
           </Form>
         </div>
 
       </div>
-
+    </Link>
   )
 }
