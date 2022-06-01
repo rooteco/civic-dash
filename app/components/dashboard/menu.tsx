a0da481f51a7875ffbf0c5dcdbc91656afa57761
andrepology <andrevacha@gmail.com>
2022-06-01 12:22:27 -0700

    revert to pretty-pause and fix grid for tablets

--- app/components/dashboard/menu.tsx
+++ app/components/dashboard/menu.tsx
@@ -1,8 +1,7 @@
 import { Link } from '@remix-run/react';
 import { useState } from 'react';
 import pause from "../../../public/assets/pause.svg";
-import ReplayIcon from '@mui/icons-material/Replay';
-
+import CachedIcon from '@mui/icons-material/Cached';
 
 
 
@@ -20,12 +19,12 @@ export default function Menu() {
 
   if (open) {
     return (
-      <div className="menu flex-row anim" style={{paddingRight: '16px'}} onMouseEnter={setOn} onMouseLeave = {setOff}>
+      <div className="menu flex-row anim" style={{ paddingRight: '16px' }} onMouseEnter={setOn} onMouseLeave={setOff}>
         <div>
-          {
-            // <img src={pause} alt="pause" className="icon-s" />
+          {true ?
+            <img src={pause} alt="pause" className="icon-s" /> :
+            <CachedIcon fontSize='small' className='pause' />
           }
-          <ReplayIcon fontSize='small' className = 'pause' />
         </div>
         <div className="flex-row">
           <strong><a href="https://rooteco.notion.site/CivicDash-v2-1-8631ce9d3fd442a1a031eec9fa48cf3b" target="_blank" rel="noreferrer">docs</a></strong>
@@ -36,12 +35,12 @@ export default function Menu() {
   }
 
   return (
-    <div className="menu flex-row" onMouseEnter={setOn} onMouseLeave = {setOff}>
+    <div className="menu flex-row" onMouseEnter={setOn} onMouseLeave={setOff}>
       <div>
-          {
-          //<img src={pause} alt="pause" className="icon-s" style = {{filter: 'drop-shadow(0px 4px 4px rgba(var(--clr-grey-200), var(--alpha-high))'}} />
-          }
-          <Replay/>
+        {true ?
+          <img src={pause} alt="pause" className="icon-s" style={{ filter: 'drop-shadow(0px 4px 4px white' }} />:
+          <CachedIcon fontSize='small' className='pause' sx={{ filter: 'drop' }} />
+        }
       </div>
     </div>
   )
@@ -59,6 +58,6 @@ const Replay = () => {
     }, 1000);
   }
 
-  return <ReplayIcon fontSize='small' className={animating? 'pause anim-rotate': 'pause'} onClick = {animate} />;
+  return <CachedIcon fontSize='small' className={animating ? 'pause anim-rotate' : 'pause'} onClick={animate} />;
 }
 
